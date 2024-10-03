40baa77c8c1113e1e2c803b9c24c37013d707d9c
anhtienpro25 <trunglh113@gmail.com>
2024-10-03 21:05:18 +0700

    ADD NEW

--- swinArm/datamodule/datamodule.py
+++ swinArm/datamodule/datamodule.py
@@ -17,8 +17,8 @@ from .vocab import vocab
 Data = List[Tuple[str, Image.Image, List[str]]]
 
 MAX_SIZE = 32e4  # change here accroading to your GPU memory
-max_n_traing_samples = 40000
-max_n_val_samples = 2000
+max_n_traing_samples = 45000
+max_n_val_samples = 1000
 
 # load data
 def data_iterator(
