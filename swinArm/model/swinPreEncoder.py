96b6a0dbcc9f2d5153371d7c21538d1dafa4a7a0
anhtienpro25 <trunglh113@gmail.com>
2024-10-06 23:26:44 +0700

    ADD NEW

--- swinArm/model/swinPreEncoder.py
+++ swinArm/model/swinPreEncoder.py
@@ -31,7 +31,7 @@ class SwinV1Encoder(pl.LightningModule):
 
         # add output layer
         self.swinv1.head = torch.nn.Sequential(
-            torch.nn.Linear(128, d_model),
+            torch.nn.Linear(192, d_model),
             torch.nn.LayerNorm(d_model),
             torch.nn.GELU(),
             torch.nn.Dropout(drop_rate),
